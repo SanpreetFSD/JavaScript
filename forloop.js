d2ecdf1b8cd0dee63f0fa55c3897660f8ccdddc0
Sanpreet Singh <sanpreet.india@gmail.com>
2023-02-11 17:00:09 +0530

    commiting session files

---
+++ forloop.js
@@ -0,0 +1,12 @@
+// var a = prompt("Enter number")
+// var sum = 0
+// var num = (a-0)
+// for(var z=1; z<=a.length; z++){
+// sum+=num%10
+// num=Math.trunc(num/10)
+// }
+// console.log(sum)
+
+for (var i=0; i<=10; i=i+2){
+    console.log(i);
+}
